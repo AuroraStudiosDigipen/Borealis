e95b85584c6c18a3eb1f6b2efb58934af3cb01db
ZyChua2000 <122003260+ZyChua2000@users.noreply.github.com>
2024-10-14 22:00:05 +0900

    Deserialiastion except for audio components and BHT

--- Borealis/src/Scene/Serialiser.cpp
+++ Borealis/src/Scene/Serialiser.cpp
@@ -628,6 +628,150 @@ namespace Borealis
 		return true;
 	}
 
+
+	static bool DeserialiseProperty(YAML::Node& data, rttr::property& prop, rttr::instance& instance)
+	{
+		auto propName = prop.get_name();
+		auto propData = data[propName.to_string().c_str()]; // entity["TransformComponent"]["Translate"]
+		auto propType = prop.get_type();
+		if (propData)
+		{
+			if (propType == rttr::type::get<int>())
+			{
+				prop.set_value(instance, propData.as<int>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<float>())
+			{
+				prop.set_value(instance, propData.as<float>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<bool>())
+			{
+				prop.set_value(instance, propData.as<bool>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<std::string>())
+			{
+				prop.set_value(instance, propData.as<std::string>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<unsigned char>())
+			{
+				prop.set_value(instance, propData.as<unsigned char>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<char>())
+			{
+				prop.set_value(instance, propData.as<char>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<unsigned short>())
+			{
+				prop.set_value(instance, propData.as<unsigned short>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<short>())
+			{
+				prop.set_value(instance, propData.as<short>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<unsigned>())
+			{
+				prop.set_value(instance, propData.as<unsigned>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<long long>())
+			{
+				prop.set_value(instance, propData.as<long long>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<unsigned long long>())
+			{
+				prop.set_value(instance, propData.as<unsigned long long>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<double>())
+			{
+				prop.set_value(instance, propData.as<double>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<glm::vec2>())
+			{
+				prop.set_value(instance, propData.as<glm::vec2>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<glm::vec3>())
+			{
+				prop.set_value(instance, propData.as<glm::vec3>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<glm::vec4>())
+			{
+				prop.set_value(instance, propData.as<glm::vec4>());
+				return true;
+			}
+
+			if (propType == rttr::type::get<Ref<Model>>())
+			{
+				prop.set_value(instance, rttr::variant(AssetManager::GetAsset<Model>(propData.as<uint64_t>())));
+				return true;
+			}
+
+			if (propType == rttr::type::get<Ref<Material>>())
+			{
+				prop.set_value(instance, rttr::variant(AssetManager::GetAsset<Material>(propData.as<uint64_t>())));
+				return true;
+			}
+
+			// other types
+		}
+
+		return false;
+	}
+
+	template <typename Component>
+	static bool DeserialiseComponent(YAML::detail::iterator_value& data, Entity& entity)
+	{
+		auto type = rttr::type::get<Component>();
+		auto componentName = type.get_name().to_string();
+		auto componentData = data[componentName];  //entity["TransformComponent"]
+
+		if (componentData)
+		{
+			if (!entity.HasComponent<Component>())
+			{
+				entity.AddComponent<Component>();
+			}
+			auto& component = entity.GetComponent<Component>();
+			ReflectionInstance rInstance(component);
+
+			auto properties = rInstance.get_type().get_properties();
+			for (auto prop : properties)
+			{
+				DeserialiseProperty(componentData, prop, rInstance);
+			}
+		}
+
+		return true;
+	}
+
+
+
 	bool Serialiser::DeserialiseScene(const std::string& filepath)
 	{
 		std::ifstream inStream(filepath);
@@ -651,149 +795,19 @@ namespace Borealis
 			for (auto entity : entities)
 			{
 				uint64_t uuid = entity["EntityID"].as<uint64_t>(); // UUID
+				Entity loadedEntity = mScene->CreateEntityWithUUID("", uuid);
 
-				auto tagComponent = entity["TagComponent"];
-				std::string name;
-				if (tagComponent)
-				{
-					name = tagComponent["Tag"].as<std::string>();
-				}
-
-				Entity loadedEntity = mScene->CreateEntityWithUUID(name, uuid);
-
-				auto transformComponent = entity["TransformComponent"];
-				if (transformComponent)
-				{
-					auto& tc = loadedEntity.GetComponent<TransformComponent>();
-					tc.Translate = entity["TransformComponent"]["Translate"].as<glm::vec3>();
-					tc.Rotation = entity["TransformComponent"]["Rotation"].as<glm::vec3>();
-					tc.Scale = entity["TransformComponent"]["Scale"].as<glm::vec3>();
-				}
-
-				auto spriteRendererComponent = entity["SpriteRendererComponent"];
-				if (spriteRendererComponent)
-				{
-					auto& src = loadedEntity.AddComponent<SpriteRendererComponent>();
-					src.Colour = entity["SpriteRendererComponent"]["Colour"].as<glm::vec4>();
-					if (entity["SpriteRendererComponent"]["Texture"].IsDefined())
-					{
-						src.Texture = AssetManager::GetAsset<Texture2D>(entity["SpriteRendererComponent"]["Texture"].as<uint64_t>());
-					}
-				}
-
-				auto circleRendererComponent = entity["CircleRendererComponent"];
-				if (circleRendererComponent)
-				{
-					auto& src = loadedEntity.AddComponent<CircleRendererComponent>();
-					src.Colour = entity["CircleRendererComponent"]["Colour"].as<glm::vec4>();
-					src.thickness = entity["CircleRendererComponent"]["Thickness"].as<float>();
-					src.fade = entity["CircleRendererComponent"]["Fade"].as<float>();
-				}
-
-				auto cameraComponent = entity["CameraComponent"];
-				if (cameraComponent)
-				{
-					auto& cc = loadedEntity.AddComponent<CameraComponent>();
-					cc.Camera.SetViewportSize(1920, 1080);
-					cc.Camera.SetCameraType((SceneCamera::CameraType)cameraComponent["Camera"]["CameraType"].as<int>());
-					cc.Camera.SetOrthoSize(cameraComponent["Camera"]["OrthoSize"].as<float>());
-					cc.Camera.SetOrthoNear(cameraComponent["Camera"]["OrthoNear"].as<float>());
-					cc.Camera.SetOrthoFar(cameraComponent["Camera"]["OrthoFar"].as<float>());
-					cc.Camera.SetPerspFOV(cameraComponent["Camera"]["PerspFOV"].as<float>());
-					cc.Camera.SetPerspNear(cameraComponent["Camera"]["PerspNear"].as<float>());
-					cc.Camera.SetPerspFar(cameraComponent["Camera"]["PerspFar"].as<float>());
-					cc.Primary = cameraComponent["Primary"].as<bool>();
-					cc.FixedAspectRatio = cameraComponent["FixedAspectRatio"].as<bool>();
-
-				}
-
-				auto meshFilterComponent = entity["MeshFilterComponent"];
-				if (meshFilterComponent)
-				{
-					auto& mfc = loadedEntity.AddComponent<MeshFilterComponent>();
-					uint64_t uuid = entity["MeshFilterComponent"]["Mesh"].as<uint64_t>(); // UUID
-					mfc.Model = AssetManager::GetAsset<Model>(uuid); // TODO: Load Mesh via UUID
-					BOREALIS_CORE_INFO(mfc.Model->mAssetHandle);
-				}
-
-				auto meshRendererComponent = entity["MeshRendererComponent"];
-				if (meshRendererComponent)
-				{
-					auto& mrc = loadedEntity.AddComponent<MeshRendererComponent>();
-					uint64_t uuid = entity["MeshRendererComponent"]["Material"].as<uint64_t>();
-					mrc.Material = AssetManager::GetAsset<Material>(uuid);
-					mrc.castShadow = meshRendererComponent["CastShadow"].as<bool>();
-				}
-
-				auto boxColliderComponent = entity["BoxColliderComponent"];
-				if (boxColliderComponent)
-				{
-					auto& bcc = loadedEntity.AddComponent<BoxColliderComponent>();
-					bcc.isTrigger = boxColliderComponent["isTrigger"].as<bool>();
-					bcc.providesContact = boxColliderComponent["providesContact"].as<bool>();
-					bcc.Material = nullptr; // TODO: Load Material via UUID
-					bcc.Center = boxColliderComponent["Center"].as<glm::vec3>();
-					bcc.Size = boxColliderComponent["Size"].as<glm::vec3>();
-				}
-
-				auto capsuleColliderComponent = entity["CapsuleColliderComponent"];
-				if (capsuleColliderComponent)
-				{
-					auto& ccc = loadedEntity.AddComponent<CapsuleColliderComponent>();
-					ccc.isTrigger = capsuleColliderComponent["isTrigger"].as<bool>();
-					ccc.providesContact = capsuleColliderComponent["providesContact"].as<bool>();
-					ccc.Material = nullptr; // TODO: Load Material via UUID
-					ccc.radius = capsuleColliderComponent["Radius"].as<float>();
-					ccc.height = capsuleColliderComponent["Height"].as<float>();
-					ccc.direction = (CapsuleColliderComponent::Direction)capsuleColliderComponent["Direction"].as<int>();
-				}
-
-				auto rigidBodyComponent = entity["RigidBodyComponent"];
-				if (rigidBodyComponent)
-				{
-					auto& rbc = loadedEntity.AddComponent<RigidBodyComponent>();
-					rbc.isBox = (RigidBodyType)rigidBodyComponent["isBox"].as<int>();
-					rbc.radius = rigidBodyComponent["Radius"].as<float>();
-					// run the update of the shape here once
-
-
-					/*rbc.mass = rigidBodyComponent["mass"].as<float>();
-					rbc.drag = rigidBodyComponent["drag"].as<float>();
-					rbc.angularDrag = rigidBodyComponent["angularDrag"].as<float>();
-					rbc.centerOfMass = rigidBodyComponent["centerOfMass"].as<glm::vec3>();
-					rbc.inertiaTensor = rigidBodyComponent["inertiaTensor"].as<glm::vec3>();
-					rbc.inertiaTensorRotation = rigidBodyComponent["inertiaTensorRotation"].as<glm::vec3>();
-					rbc.AutomaticCenterOfMass = rigidBodyComponent["AutomaticCenterOfMass"].as<bool>();
-					rbc.AutomaticTensor = rigidBodyComponent["AutomaticTensor"].as<bool>();
-					rbc.useGravity = rigidBodyComponent["useGravity"].as<bool>();
-					rbc.isKinematic = rigidBodyComponent["isKinematic"].as<bool>();*/
-				}
-
-				auto lightComponent = entity["LightComponent"];
-				if (lightComponent)
-				{
-					auto& lc = loadedEntity.AddComponent<LightComponent>();
-
-					lc.ambient = lightComponent["Ambient"].as<glm::vec3>();
-					lc.diffuse = lightComponent["Diffuse"].as<glm::vec3>();
-					lc.direction = lightComponent["Direction"].as<glm::vec3>();
-					lc.specular = lightComponent["Specular"].as<glm::vec3>();
-					lc.linear = lightComponent["Linear"].as<float>();
-					lc.quadratic = lightComponent["Quadratic"].as<float>();
-					lc.type = (LightComponent::Type)lightComponent["Type"].as<int>();
-					lc.InnerOuterSpot.x = lightComponent["InnerSpotX"].as<float>();
-					lc.InnerOuterSpot.y = lightComponent["InnerSpotY"].as<float>();
-
-					/*lc.Colour = lightComponent["Colour"].as<glm::vec4>();
-					lc.InnerOuterSpot = glm::vec2(lightComponent["InnerSpot"].as<float>(), lightComponent["OuterSpot"].as<float>());
-					lc.Temperature = lightComponent["Temperature"].as<float>();
-					lc.Intensity = lightComponent["Intensity"].as<float>();
-					lc.IndirectMultiplier = lightComponent["IndirectMultiplier"].as<float>();
-					lc.Range = lightComponent["Range"].as<float>();
-					lc.type = (LightComponent::Type)lightComponent["Type"].as<int>();
-					lc.shadowType = (LightComponent::ShadowType)lightComponent["ShadowType"].as<int>();
-					lc.lightAppearance = (LightComponent::LightAppearance)lightComponent["LightAppearance"].as<int>();*/
-				}
+				DeserialiseComponent<TagComponent>(entity, loadedEntity);
+				DeserialiseComponent<TransformComponent>(entity, loadedEntity);
+				DeserialiseComponent<SpriteRendererComponent>(entity, loadedEntity);
+				DeserialiseComponent<CircleRendererComponent>(entity, loadedEntity);
+				//DeserialiseComponent<CameraComponent>(entity, loadedEntity);
+				DeserialiseComponent<MeshFilterComponent>(entity, loadedEntity);
+				DeserialiseComponent<MeshRendererComponent>(entity, loadedEntity);
+				DeserialiseComponent<BoxColliderComponent>(entity, loadedEntity);
+				DeserialiseComponent<CapsuleColliderComponent>(entity, loadedEntity);
+				DeserialiseComponent<RigidBodyComponent>(entity, loadedEntity);
+				DeserialiseComponent<LightComponent>(entity, loadedEntity);
 				auto behaviourTreeComponent = entity["BehaviourTreeComponent"];
 				/*
 					extract the name of tree and root node, then iteritivly build the tree, then call the clone method by createfromname function

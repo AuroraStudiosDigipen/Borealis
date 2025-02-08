496a890b2923e5a9abe977c60440bbd68877a211
vkohxy <112920572+vkohxy@users.noreply.github.com>
2025-02-09 07:01:37 +0800

    removed bgm test

--- Borealis/src/Core/ApplicationManager.cpp
+++ Borealis/src/Core/ApplicationManager.cpp
@@ -82,11 +82,6 @@ namespace Borealis
 		ULONGLONG prevTickCount = GetTickCount64(); // Get initial tick count
 		PROFILE_FUNCTION();
 
-		Audio audio = AudioEngine::LoadAudio("MoodSwingsBGM.wav");
-		Ref<Audio> ref = MakeRef<Audio>(audio);
-		int ch = AudioEngine::Play(ref, {}, 1.f, true, "BGM");
-		AudioEngine::ApplyFadeIn(ch, 3.f, 1.f);
-
 		float count = 0.f;
 
 		while (mIsRunning)

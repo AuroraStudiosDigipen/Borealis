78d2740837abb614fb8bad2995af7d9cda677e1b
gordonchangg <guogeng98@gmail.com>
2025-03-18 14:08:52 +0800

    added outline for fonts

--- BorealisAssetCompiler/src/Importer/FontImporter.cpp
+++ BorealisAssetCompiler/src/Importer/FontImporter.cpp
@@ -189,15 +189,16 @@ namespace BorealisAssetCompiler
 						charSet.add(c);
 					}
 				}
-				double fontScale = 4.0; // let meta decide in the future
+				double fontScale = 8.0; // let meta decide in the future
 				msdf_atlas::FontGeometry fontGeometry = msdf_atlas::FontGeometry(&glyphs);
 				int loadedGlyphs = fontGeometry.loadCharset(font, fontScale, charSet);
 
 
 				msdf_atlas::TightAtlasPacker packer;
-				packer.setPixelRange(2.0);
+				//packer.setPixelRange(6.5);
+				packer.setUnitRange(0.5);
 				packer.setMiterLimit(1.0);
-				packer.setScale(40.0);
+				packer.setScale(48.0);
 				int remain = packer.pack(glyphs.data(), (int)glyphs.size());
 
 				int width, height;
--- BorealisEditor/engineResources/Shaders/Renderer2D_Font.glsl
+++ BorealisEditor/engineResources/Shaders/Renderer2D_Font.glsl
@@ -37,8 +37,10 @@ flat in int v_EntityID;
 			
 uniform sampler2D u_Texture;
 
+float u_outline = 0.23;
+
 float screenPxRange() {
-	const float pxRange = 2.0; // set to distance field's pixel range
+	float pxRange = 100.f; // set to distance field's pixel range
     vec2 unitRange = vec2(pxRange)/vec2(textureSize(u_Texture, 0));
     vec2 screenTexSize = vec2(1.0)/fwidth(v_TexCoord);
     return max(0.5*dot(unitRange, screenTexSize), 1.0);
@@ -50,17 +52,36 @@ float median(float r, float g, float b) {
 
 void main()
 {
-	entityIDs = v_EntityID;
+    entityIDs = v_EntityID;
 
-	vec3 msd = texture(u_Texture, v_TexCoord).rgb;
-	float sd = median(msd.r, msd.g, msd.b);
-	float screenPxDistance = screenPxRange()*(sd - 0.5);
-	float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
-	if (opacity == 0.0)
-		discard;
+    vec3 msd = texture(u_Texture, v_TexCoord).rgb;
+    float sd = median(msd.r, msd.g, msd.b);
+    float width = screenPxRange();
 
-	vec4 bgColor = vec4(0.0);
-    color = mix(bgColor, v_Color, opacity);	
-	if (color.a == 0.0)
-		discard;
+    float relativeOutline = 48.0 / 16.0; 
+    float outlineWidth = mix(0.75, u_outline, 0.5);
+
+    float inner = width * (sd - 0.5) + 0.5;
+    float outer = width * (sd - 0.5 + outlineWidth) + 0.5;
+
+    float inner_opacity = clamp(inner, 0.0, 1.0);
+    vec4 inner_color = vec4(1, 1, 1, 1);
+    float outer_opacity = clamp(outer, 0.0, 1.0);
+    vec4 outer_color = vec4(0, 0, 0, 1); 
+
+    color = (inner_color * inner_opacity) + (outer_color * outer_opacity);
+
+	// entityIDs = v_EntityID;
+
+	// vec3 msd = texture(u_Texture, v_TexCoord).rgb;
+	// float sd = median(msd.r, msd.g, msd.b);
+	// float screenPxDistance = screenPxRange()*(sd - 0.5);
+	// float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
+	// if (opacity == 0.0)
+	// 	discard;
+
+	// vec4 bgColor = vec4(0.0);
+    // color = mix(bgColor, v_Color, opacity);	
+	// if (color.a == 0.0)
+	// 	discard;
 }

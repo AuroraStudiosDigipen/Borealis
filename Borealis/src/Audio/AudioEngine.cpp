652f9acc44f0681ebfbda1b3dcef31dad17f53e6
ZyChua2000 <122003260+ZyChua2000@users.noreply.github.com>
2025-03-21 11:31:49 +0800

    Update AudioEngine.cpp

--- Borealis/src/Audio/AudioEngine.cpp
+++ Borealis/src/Audio/AudioEngine.cpp
@@ -25,6 +25,180 @@ prior written consent of DigiPen Institute of Technology is prohibited.
 
 namespace Borealis
 {
+
+    static std::string FMODError2String(FMOD_RESULT result)
+    {
+        switch (result)
+        {
+        case FMOD_ERR_INVALID_FLOAT:
+            return "FMOD_ERR_INVALID_FLOAT";
+        case FMOD_ERR_INVALID_HANDLE:
+            return "FMOD_ERR_INVALID_HANDLE";
+        case FMOD_ERR_INVALID_PARAM:
+            return "FMOD_ERR_INVALID_PARAM";
+        case FMOD_ERR_INVALID_POSITION:
+            return "FMOD_ERR_INVALID_POSITION";
+        case FMOD_ERR_EVENT_NOTFOUND:
+            return "FMOD_ERR_EVENT_NOTFOUND";
+        case FMOD_OK:
+            return "FMOD_OK";
+        case FMOD_ERR_BADCOMMAND:
+            return "FMOD_ERR_BADCOMMAND";
+        case FMOD_ERR_CHANNEL_ALLOC:
+            return "FMOD_ERR_CHANNEL_ALLOC";
+        case FMOD_ERR_CHANNEL_STOLEN:
+            return "FMOD_ERR_CHANNEL_STOLEN";
+        case FMOD_ERR_DMA:
+            return "FMOD_ERR_DMA";
+        case FMOD_ERR_DSP_CONNECTION:
+            return "FMOD_ERR_DSP_CONNECTION";
+        case FMOD_ERR_DSP_DONTPROCESS:
+            return "FMOD_ERR_DSP_DONTPROCESS";
+        case FMOD_ERR_DSP_FORMAT:
+            return "FMOD_ERR_DSP_FORMAT";
+        case FMOD_ERR_DSP_INUSE:
+            return "FMOD_ERR_DSP_INUSE";
+        case FMOD_ERR_DSP_NOTFOUND:
+            return "FMOD_ERR_DSP_NOTFOUND";
+        case FMOD_ERR_DSP_RESERVED:
+            return "FMOD_ERR_DSP_RESERVED";
+        case FMOD_ERR_DSP_SILENCE:
+            return "FMOD_ERR_DSP_SILENCE";
+        case FMOD_ERR_DSP_TYPE:
+            return "FMOD_ERR_DSP_TYPE";
+        case FMOD_ERR_FILE_BAD:
+            return "FMOD_ERR_FILE_BAD";
+        case FMOD_ERR_FILE_COULDNOTSEEK:
+            return "FMOD_ERR_FILE_COULDNOTSEEK";
+        case FMOD_ERR_FILE_DISKEJECTED:
+            return "FMOD_ERR_FILE_DISKEJECTED";
+        case FMOD_ERR_FILE_EOF:
+            return "FMOD_ERR_FILE_EOF";
+        case FMOD_ERR_FILE_ENDOFDATA:
+            return "FMOD_ERR_FILE_ENDOFDATA";
+        case FMOD_ERR_FILE_NOTFOUND:
+            return "FMOD_ERR_FILE_NOTFOUND";
+        case FMOD_ERR_FORMAT:
+            return "FMOD_ERR_FORMAT";
+        case FMOD_ERR_HEADER_MISMATCH:
+            return "FMOD_ERR_HEADER_MISMATCH";
+        case FMOD_ERR_HTTP:
+            return "FMOD_ERR_HTTP";
+        case FMOD_ERR_HTTP_ACCESS:
+            return "FMOD_ERR_HTTP_ACCESS";
+        case FMOD_ERR_HTTP_PROXY_AUTH:
+            return "FMOD_ERR_HTTP_PROXY_AUTH";
+        case FMOD_ERR_HTTP_SERVER_ERROR:
+            return "FMOD_ERR_HTTP_SERVER_ERROR";
+        case FMOD_ERR_HTTP_TIMEOUT:
+            return "FMOD_ERR_HTTP_TIMEOUT";
+        case FMOD_ERR_INITIALIZATION:
+            return "FMOD_ERR_INITIALIZATION";
+        case FMOD_ERR_INITIALIZED:
+            return "FMOD_ERR_INITIALIZED";
+        case FMOD_ERR_INTERNAL:
+            return "FMOD_ERR_INTERNAL";
+        case FMOD_ERR_INVALID_SPEAKER:
+            return "FMOD_ERR_INVALID_SPEAKER";
+        case FMOD_ERR_INVALID_SYNCPOINT:
+            return "FMOD_ERR_INVALID_SYNCPOINT";
+        case FMOD_ERR_INVALID_THREAD:
+            return "FMOD_ERR_INVALID_THREAD";
+        case FMOD_ERR_INVALID_VECTOR:
+            return "FMOD_ERR_INVALID_VECTOR";
+        case FMOD_ERR_MAXAUDIBLE:
+            return "FMOD_ERR_MAXAUDIBLE";
+        case FMOD_ERR_MEMORY:
+            return "FMOD_ERR_MEMORY";
+        case FMOD_ERR_MEMORY_CANTPOINT:
+            return "FMOD_ERR_MEMORY_CANTPOINT";
+        case FMOD_ERR_NEEDS3D:
+            return "FMOD_ERR_NEEDS3D";
+        case FMOD_ERR_NEEDSHARDWARE:
+            return "FMOD_ERR_NEEDSHARDWARE";
+        case FMOD_ERR_NET_CONNECT:
+            return "FMOD_ERR_NET_CONNECT";
+        case FMOD_ERR_NET_SOCKET_ERROR:
+            return "FMOD_ERR_NET_SOCKET_ERROR";
+        case FMOD_ERR_NET_URL:
+            return "FMOD_ERR_NET_URL";
+        case FMOD_ERR_NET_WOULD_BLOCK:
+            return "FMOD_ERR_NET_WOULD_BLOCK";
+        case FMOD_ERR_NOTREADY:
+            return "FMOD_ERR_NOTREADY";
+        case FMOD_ERR_OUTPUT_ALLOCATED:
+            return "FMOD_ERR_OUTPUT_ALLOCATED";
+        case FMOD_ERR_OUTPUT_CREATEBUFFER:
+            return "FMOD_ERR_OUTPUT_CREATEBUFFER";
+        case FMOD_ERR_OUTPUT_DRIVERCALL:
+            return "FMOD_ERR_OUTPUT_DRIVERCALL";
+        case FMOD_ERR_OUTPUT_FORMAT:
+            return "FMOD_ERR_OUTPUT_FORMAT";
+        case FMOD_ERR_OUTPUT_INIT:
+            return "FMOD_ERR_OUTPUT_INIT";
+        case FMOD_ERR_OUTPUT_NODRIVERS:
+            return "FMOD_ERR_OUTPUT_NODRIVERS";
+        case FMOD_ERR_PLUGIN:
+            return "FMOD_ERR_PLUGIN";
+        case FMOD_ERR_PLUGIN_MISSING:
+            return "FMOD_ERR_PLUGIN_MISSING";
+        case FMOD_ERR_PLUGIN_RESOURCE:
+            return "FMOD_ERR_PLUGIN_RESOURCE";
+        case FMOD_ERR_PLUGIN_VERSION:
+            return "FMOD_ERR_PLUGIN_VERSION";
+        case FMOD_ERR_RECORD:
+            return "FMOD_ERR_RECORD";
+        case FMOD_ERR_REVERB_CHANNELGROUP:
+            return "FMOD_ERR_REVERB_CHANNELGROUP";
+        case FMOD_ERR_REVERB_INSTANCE:
+            return "FMOD_ERR_REVERB_INSTANCE";
+        case FMOD_ERR_SUBSOUNDS:
+            return "FMOD_ERR_SUBSOUNDS";
+        case FMOD_ERR_SUBSOUND_ALLOCATED:
+            return "FMOD_ERR_SUBSOUND_ALLOCATED";
+        case FMOD_ERR_SUBSOUND_CANTMOVE:
+            return "FMOD_ERR_SUBSOUND_CANTMOVE";
+        case FMOD_ERR_TAGNOTFOUND:
+            return "FMOD_ERR_TAGNOTFOUND";
+        case FMOD_ERR_TOOMANYCHANNELS:
+            return "FMOD_ERR_TOOMANYCHANNELS";
+        case FMOD_ERR_TRUNCATED:
+            return "FMOD_ERR_TRUNCATED";
+        case FMOD_ERR_UNIMPLEMENTED:
+            return "FMOD_ERR_UNIMPLEMENTED";
+        case FMOD_ERR_UNINITIALIZED:
+            return "FMOD_ERR_UNINITIALIZED";
+        case FMOD_ERR_UNSUPPORTED:
+            return "FMOD_ERR_UNSUPPORTED";
+        case FMOD_ERR_VERSION:
+            return "FMOD_ERR_VERSION";
+        case FMOD_ERR_EVENT_ALREADY_LOADED:
+            return "FMOD_ERR_EVENT_ALREADY_LOADED";
+        case FMOD_ERR_EVENT_LIVEUPDATE_BUSY:
+            return "FMOD_ERR_EVENT_LIVEUPDATE_BUSY";
+        case FMOD_ERR_EVENT_LIVEUPDATE_MISMATCH:
+            return "FMOD_ERR_EVENT_LIVEUPDATE_MISMATCH";
+        case FMOD_ERR_EVENT_LIVEUPDATE_TIMEOUT:
+            return "FMOD_ERR_EVENT_LIVEUPDATE_TIMEOUT";
+        case FMOD_ERR_STUDIO_UNINITIALIZED:
+            return "FMOD_ERR_STUDIO_UNINITIALIZED";
+        case FMOD_ERR_STUDIO_NOT_LOADED:
+            return "FMOD_ERR_STUDIO_NOT_LOADED";
+        case FMOD_ERR_INVALID_STRING:
+            return "FMOD_ERR_INVALID_STRING";
+        case FMOD_ERR_ALREADY_LOCKED:
+            return "FMOD_ERR_ALREADY_LOCKED";
+        case FMOD_ERR_NOT_LOCKED:
+            return "FMOD_ERR_NOT_LOCKED";
+        case FMOD_ERR_RECORD_DISCONNECTED:
+            return "FMOD_ERR_RECORD_DISCONNECTED";
+        case FMOD_ERR_TOOMANYSAMPLES:
+            return "FMOD_ERR_TOOMANYSAMPLES";
+        default:
+            return "FMOD_ERR_UNKNOWN";
+        }
+    }
+
     static bool HasFileChanged(const std::wstring& filePath, FILETIME& lastWriteTime) {
         HANDLE hFile = CreateFileW(filePath.c_str(), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
             NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -83,7 +257,7 @@ namespace Borealis
     {
         if (result != FMOD_OK)
         {
-            BOREALIS_CORE_ERROR("FMOD: {}", result);
+            BOREALIS_CORE_ERROR("FMOD: {}", FMODError2String(result));
             return 1;
         }
         return 0;
@@ -488,6 +662,8 @@ namespace Borealis
 
     int AudioEngine::Play(std::array<uint8_t, 16> id, const glm::mat4& transform, std::unordered_map<std::string, float> parameterMap)
     {
+
+        int error = 0;
         auto position = glm::vec3(transform[3]);
         glm::vec3 forward = glm::normalize(glm::vec3(transform[2]));
         glm::vec3 up = glm::normalize(glm::vec3(transform[1]));
@@ -498,28 +674,31 @@ namespace Borealis
         FMOD::Studio::EventInstance* eventInstance;
         FMOD::Studio::EventDescription* eventDesc;
         FMOD_GUID guid = std::bit_cast<FMOD_GUID>(id);
-        ErrorCheck(sgpImplementation->mpStudioSystem->getEventByID(&guid, &eventDesc));
-        eventDesc->createInstance(&eventInstance);
+        error = std::max(error,ErrorCheck(sgpImplementation->mpStudioSystem->getEventByID(&guid, &eventDesc)));
+        error = std::max(error, ErrorCheck(eventDesc->createInstance(&eventInstance)));
         FMOD_3D_ATTRIBUTES attr;
         attr.position = VectorToFmod(position);
-        //attr.forward = fmodForward;
-        //attr.up = fmodUp;
-        //attr.velocity = { 0.0f, 0.0f, 0.0f };
-        ErrorCheck(eventInstance->set3DAttributes(&attr));
-        ErrorCheck(eventInstance->setPaused(false));
+        attr.forward = fmodForward;
+        attr.up = fmodUp;
+        attr.velocity = { 0.0f, 0.0f, 0.0f };
+        error = std::max(error, ErrorCheck(eventInstance->set3DAttributes(&attr)));
+        error = std::max(error, ErrorCheck(eventInstance->setPaused(false)));
         for (auto& [paramName, value] : parameterMap)
         {
             FMOD_STUDIO_PARAMETER_DESCRIPTION paramDesc;
             if (ErrorCheck(eventDesc->getParameterDescriptionByName(paramName.c_str(), &paramDesc)) == 0)
             {
-                ErrorCheck(eventInstance->setParameterByName(paramName.c_str(), value));
+                error = std::max(0, ErrorCheck(eventInstance->setParameterByName(paramName.c_str(), value)));
             }
         }
-        ErrorCheck(eventInstance->start());
+        error = std::max(error, ErrorCheck(eventInstance->start()));
 
        
 
-        
+        if (error)
+        {
+            BOREALIS_CORE_ERROR("Error playing Event {}", GuidToString(id));
+        }
 
         sgpImplementation->mChannels[sgpImplementation->mnNextChannelId++] = eventInstance;
 
@@ -533,29 +712,34 @@ namespace Borealis
         glm::vec3 up = glm::normalize(glm::vec3(transform[1]));
         FMOD_VECTOR fmodForward = VectorToFmod(forward);
         FMOD_VECTOR fmodUp = VectorToFmod(up);
-
+        int error = 0;
 
         FMOD::Studio::EventInstance* eventInstance;
         FMOD::Studio::EventDescription* eventDesc;
         FMOD_GUID guid = std::bit_cast<FMOD_GUID>(id);
-        ErrorCheck(sgpImplementation->mpStudioSystem->getEventByID(&guid, &eventDesc));
-        eventDesc->createInstance(&eventInstance);
+        error = std::max(error, ErrorCheck(sgpImplementation->mpStudioSystem->getEventByID(&guid, &eventDesc)));
+        error = std::max(error, ErrorCheck(eventDesc->createInstance(&eventInstance)));
         FMOD_3D_ATTRIBUTES attr;
         attr.position = VectorToFmod(position);
-       // attr.forward = fmodForward;
-        //attr.up = fmodUp;
-       // attr.velocity = { 0.0f, 0.0f, 0.0f };
-        ErrorCheck(eventInstance->set3DAttributes(&attr));
-        ErrorCheck(eventInstance->setPaused(false));
+        attr.forward = fmodForward;
+        attr.up = fmodUp;
+        attr.velocity = { 0.0f, 0.0f, 0.0f };
+        error = std::max(error, ErrorCheck(eventInstance->set3DAttributes(&attr)));
+        error = std::max(error, ErrorCheck(eventInstance->setPaused(false)));
         for (auto& [paramName, value] : parameterMap)
         {
             FMOD_STUDIO_PARAMETER_DESCRIPTION paramDesc;
             if (ErrorCheck(eventDesc->getParameterDescriptionByName(paramName.c_str(), &paramDesc)) == 0)
             {
-                ErrorCheck(eventInstance->setParameterByNameWithLabel(paramName.c_str(), value.c_str()));
+                error = std::max(error, ErrorCheck(eventInstance->setParameterByNameWithLabel(paramName.c_str(), value.c_str())));
             }
         }
-        ErrorCheck(eventInstance->start());
+        error = std::max(error, ErrorCheck(eventInstance->start()));
+
+        if (error)
+        {
+            BOREALIS_CORE_ERROR("Error playing Event {}", GuidToString(id));
+        }
 
         sgpImplementation->mChannels[sgpImplementation->mnNextChannelId++] = eventInstance;
         return sgpImplementation->mnNextChannelId - 1; // Return the channel ID for tracking
